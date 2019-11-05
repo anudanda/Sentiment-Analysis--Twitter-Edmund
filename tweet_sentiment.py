f1539ce444a830479d444f7a10eb7105a72c82a9
anudanda <anusha.danda@gmail.com>
2019-11-05 13:13:08 -0600

    Update tweet_sentiment.py

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,17 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 08 11:57:32 2016
+
+@author: a_danda
+"""
+
+
+
+import json
+import re
 import sys
+from pprint import pprint
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
 
-def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
 
-if __name__ == '__main__':
-    main()
+
+data = []
+with open(sys.argv[2]) as f:
+    for line in f:
+        data.append(json.loads(line))
+#pprint(data)
+
+    
+afinnfile = open(sys.argv[1])
+scores = {} # initialize an empty dictionary
+for line in afinnfile:
+  term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+  scores[term] = int(score)  # Convert the score to an integer.
+  
+#print scores.items() # Print every (term, score) pair in the dictionary
+      
+
+
+
+value = 0
+word_list = []
+tweetscore_list = []
+for dict_n in data:
+        key =  dict_n.keys()
+        if len(key) > 1:
+            
+            key1 = key[2]
+            
+            tweet = dict_n[key1]
+            if tweet is not None:
+                tweet = tweet.lower()
+                word_list = re.findall(r'\w+', tweet ,flags = re.UNICODE | re.LOCALE) 
+            
+            
+                """print word_list"""
+                for word in word_list:
+                
+                    if word.encode('utf-8') in scores.keys():
+                        #print word  
+                        value = value+scores[word]
+                
+                        return(value)
+                value = 0    
+        
+#print tweetscore_list
